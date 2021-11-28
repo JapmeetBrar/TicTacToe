02c2d4e8d6f8a0f884141b9383c3aedc0e6934d8
Jap Laptop <Japmeetb321@gmail.com>
2021-11-28 14:24:01 -0500

    writeToBoard function complete

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const gameBoard = (() => {
-    let gameboard = [];
+const GameBoard = (function() {
+    let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+    
+    return {
+        board
+    };
 })();
+
+const DisplayController = (function(){
+    const writeToBoard = (player, position) => {
+        let square = document.querySelector(`[data-square = "${position}"]`);
+        if (square.textContent !== "") return;
+        console.log("written")
+        square.textContent = "X"
+    }
+    return {writeToBoard};
+})();
+
+const Player = (name) =>{
+    const getName = () => console.log("Hi I am " + name);
+
+    return{
+        getName
+    };
+}
+
+console.log(GameBoard.board);
+
+let jap = Player('jap');
+
+DisplayController.writeToBoard(jap, 8)
