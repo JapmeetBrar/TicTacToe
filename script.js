23f7f603bce3992d30b3ac786681c8cee035a6ca
Jap Laptop <Japmeetb321@gmail.com>
2021-11-29 00:23:48 -0500

    Board working, no way to check for a win yet

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const GameBoard = (function() {
-    let board = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]];
+    let board = [["", "", ""], ["", "", ""], ["", "", ""]];
     
     return {
         board
@@ -9,23 +9,49 @@ const GameBoard = (function() {
 const DisplayController = (function(){
     const writeToBoard = (player, position) => {
         let square = document.querySelector(`[data-square = "${position}"]`);
-        if (square.textContent !== "") return;
-        console.log("written")
-        square.textContent = "X"
+        
+        square.textContent = player.symbol;
     }
     return {writeToBoard};
 })();
 
-const Player = (name) =>{
+const mainGame = (function(){
+    let squares = document.querySelectorAll('.block');
+    let player1Move = true;
+
+    squares.forEach((square)=>{
+        square.addEventListener('click', ()=>{
+            (player1Move)?_playMove(player1, square):_playMove(player2, square)
+        })
+    })
+
+    const _playMove = (player, square) => {
+        if (square.textContent !== "") return;
+        let currentSpot = square.dataset.square;
+        let size = GameBoard.board.length;
+        let row;
+        for (let i = 1; i <= size; i++){
+            if (currentSpot < size*i){
+                row = i-1;
+                break;
+            }
+        }
+        GameBoard.board[row][currentSpot-size*row] = "1";
+        DisplayController.writeToBoard(player, currentSpot);
+        player1Move = !player1Move;
+    }
+})();
+
+const Player = (name, sign) =>{
     const getName = () => console.log("Hi I am " + name);
 
+    let symbol = sign;
+
     return{
-        getName
+        getName,
+        symbol
     };
 }
 
-console.log(GameBoard.board);
-
-let jap = Player('jap');
-
-DisplayController.writeToBoard(jap, 8)
+let player1 = Player('jap', 'X');
+let player2 = Player('john', 'O')
